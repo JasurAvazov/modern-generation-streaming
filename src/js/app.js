26117f0324456eaf8dfa61428132f4079d385382
JasurAvazov <jasuravazov4@gmail.com>
2023-06-06 19:38:06 +0500

    init

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,13 @@
+// modules
 import * as functions from "./modules/functions.js";
+import * as select from "./modules/select.js";
 
+//components
+import * as quiz from "./components/quiz.js"
+
+// modules
 functions.isWebp();
+select.init();
 
-// import Swiper, { Navigation, Pagination } from 'swiper';
-
-// const swiper = new Swiper();
+//components
+quiz.init()
