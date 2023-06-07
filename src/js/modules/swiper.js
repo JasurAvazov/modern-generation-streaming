dea0b68770a6e191615f89d36a5634ce46c3f598
JasurAvazov <jasuravazov4@gmail.com>
2023-06-07 12:52:39 +0500

    new intro and header

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -1,3 +1,19 @@
-export function init(){
-    console.log("hi");
+import Swiper, { Navigation, Pagination } from "swiper";
+
+export function init() {
+	var swiper = new Swiper(".intro-swiper", {
+		modules: [Pagination, Navigation],
+		slidesPerView: "auto",
+		spaceBetween: 20,
+		centeredSlides: true,
+		grabCursor: true,
+		pagination: {
+			el: ".intro-pagination",
+			type: "fraction",
+		},
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+	});
 }
